2c29d35830064c0c6ded4da367696dc97b4a3b1f
nl020 <nl020@bucknell.edu>
2024-04-04 14:50:13 -0400

    Complete Project 7

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# Task-Project-7
-An Efficient Task Management Application for iOS
+# Project 7 - *Task Management*
+
+Submitted by: **Nolan Lwin**
+
+In this project, I updated an existing Task app with the ability to save and persist task data to local storage using UserDefaults. In addition I added tab bar navigation so users can manage their tasks on the main tasks list as well as a calendar.
+
+Time spent: **3** hours spent in total
+
+## Required Features
+
+The following **required** functionalities are completed:
+
+1. Setup
+
+- [x] Familiarize with the ```Task``` data model.
+
+2. User can create, read, and update tasks from local storage
+
+- [x] Save an array of tasks to UserDefaults. (type method)
+- [x] Retrieve an array of saved tasks from UserDefaults. (type method)
+- [x] Add a new task or update an existing task with the current task. (instance method)
+
+3. User can access the Calendar feature via tab navigation
+
+- [x] Add a tab bar controller and connect it to both the main "Task" list feature and the "Calendar" feature.
+- [x] Configure the tab bar item's for each tab with a title and image.
+
+## Video Walkthrough
+
+
+## License
+
+    Copyright 2024 Nolan Lwin
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
---
+++ ios101-project7-task/Task.swift
@@ -0,0 +1,76 @@
+//
+//  Task.swift
+//
+
+import UIKit
+
+// The Task model
+struct Task: Codable {
+    // The task's title
+    var title: String
+    // An optional note
+    var note: String?
+    // The due date by which the task should be completed
+    var dueDate: Date
+    // A boolean to determine if the task has been completed. Defaults to `false`
+    var isComplete: Bool = false {
+        // Any time a task is completed, update the completedDate accordingly.
+        didSet {
+            if isComplete {
+                // The task has just been marked complete, set the completed date to "right now".
+                completedDate = Date()
+            } else {
+                completedDate = nil
+            }
+        }
+    }
+    // The date the task was completed
+    // private(set) means this property can only be set from within this struct, but read from anywhere (i.e. public)
+    private(set) var completedDate: Date?
+    // The date the task was created
+    var createdDate: Date
+    // An id (Universal Unique Identifier) used to identify a task.
+    var id: String
+
+    // Initialize a new task
+    // `note` and `dueDate` properties have default values provided if none are passed into the init by the caller.
+    init(title: String, note: String? = nil, dueDate: Date = Date()) {
+        self.title = title
+        self.note = note
+        self.dueDate = dueDate
+        self.createdDate = Date() // Initialize createdDate with current date
+        self.id = UUID().uuidString // Initialize id with a UUID
+    }
+
+    // MARK: - Task + UserDefaults
+    static func save(_ tasks: [Task]) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(tasks) {
+            UserDefaults.standard.set(encoded, forKey: "tasks")
+        }
+    }
+
+    static func getTasks() -> [Task] {
+        if let savedTasks = UserDefaults.standard.object(forKey: "tasks") as? Data {
+            let decoder = JSONDecoder()
+            if let loadedTasks = try? decoder.decode([Task].self, from: savedTasks) {
+                return loadedTasks
+            }
+        }
+        return []
+    }
+
+    func save() {
+        var tasks = Task.getTasks()
+        if let existingIndex = tasks.firstIndex(where: { $0.id == self.id }) {
+            // Task already exists, update it
+            tasks[existingIndex] = self
+        } else {
+            // Task doesn't exist, add it
+            if !tasks.contains(where: { $0.id == self.id }) {
+                tasks.append(self)
+            }
+        }
+        Task.save(tasks)
+    }
+}
